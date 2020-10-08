e751522bbffacb2d3f5f360f3f14da816ce67c04
Leonard Ulman <namludranoel@gmail.com>
2020-10-08 17:00:06 +0200

    Initial commit

---
+++ caesar/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace caesar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            int x = 0;
+            try
+            {
+                x = int.Parse(key.Text);
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            output.Text = Program.Encrypt(input.Text, x);
+        }
+    }
+}
---
+++ caesar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caesar
+{
+    class Program
+    {
+        static char[] alphabet = { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż' };
+        public static string Encrypt(string input, int key)
+        {
+            string output = string.Empty;
+            input.ToLower();
+            foreach (char ch in input)
+            {
+                for(int i = 0; i < 33; i++)
+                    if (ch == alphabet[i])
+                        output += alphabet[(i + key) % 34];
+            }
+
+            return output;
+        }
+    }
+}
