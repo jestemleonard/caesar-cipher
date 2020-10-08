9b8cd7fe6d5ac5709a7c28fcfd1f9df084859760
Leonard Ulman <namludranoel@gmail.com>
2020-10-09 01:49:32 +0200

    Corrected decription

--- caesar/Program.cs
+++ caesar/Program.cs
@@ -8,16 +8,16 @@ namespace caesar
 {
     class Program
     {
-        static char[] alphabet = { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż' };
+        static char[] alphabet = { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż' };
         public static string Encrypt(string input, int key)
         {
             string output = string.Empty;
             input = input.ToLower();
             foreach (char ch in input)
             {
-                for(int i = 0; i < 34; i++)
+                for(int i = 0; i < 35; i++)
                     if (ch == alphabet[i])
-                        output += alphabet[(i + key) % 34];
+                        output += alphabet[((i + key) % 35)];
             }
 
             return output;
@@ -29,11 +29,14 @@ namespace caesar
             input = input.ToLower();
             foreach (char ch in input)
             {
-                for (int i = 0; i < 34; i++)
+                for (int i = 0; i < 35; i++)
                     if (ch == alphabet[i])
                     {
-                        int absoluteValue = Math.Abs(i - key);
-                        output += alphabet[(absoluteValue) % 34];
+                        int value = i - (key % 35);
+                        if (value < 0)
+                            output += alphabet[35 + value];
+                        else
+                            output += alphabet[value];
                     }
             }
 
